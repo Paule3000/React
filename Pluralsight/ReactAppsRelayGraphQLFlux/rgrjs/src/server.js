f30406e3ac5a947b0d636940e7ec51fa2422ee87
blah <blah@blah.com>
2018-07-03 13:09:54 +0100

    ES6func

--- Pluralsight/ReactAppsRelayGraphQLFlux/rgrjs/src/server.js
+++ Pluralsight/ReactAppsRelayGraphQLFlux/rgrjs/src/server.js
@@ -10,31 +10,24 @@ let url = 'mongodb://localhost:27017';
 const dbName = 'rgrjs';
 let db;
 
-// MongoClient.connect(url, (err, client) => {
-//     console.log("err: " + err + "db: " + client);
-//     if (err) {
-//         console.log("Connect: " + err);
-//         throw err;
-//     }
+MongoClient.connect(url, (err, client) => {
+    console.log("err: " + err + "db: " + client);
+    if (err) {
+        console.log("Connect: " + err);
+        throw err;
+    }
 
-//     db = client.db('rgrjs');
+    db = client.db('rgrjs');
+    app.listen(3000, () => console.log('listening on port 3000'));
+});
+
+// MongoClient.connect(url, function(err, client) {
+//   console.log("Connected successfully to server");
+
+//     db = client.db(dbName);
+
 //     app.listen(3000, () => console.log('listening on port 3000'));
 // });
-
-MongoClient.connect(url, function(err, client) {
-//   assert.equal(null, err);
-  console.log("Connected successfully to server");
-
-//   const db = client.db(dbName);
-    db = client.db(dbName);
-
-    app.listen(3000, () => console.log('listening on port 3000'));
-//   insertDocuments(db, function() {
-    // findDocuments(db, function() {
-    //   client.close();
-    // });
-//   });
-});
 
 app.get("/data/links", (req, res) => {
     db.collection("links").find({}).toArray((err, links) => {
