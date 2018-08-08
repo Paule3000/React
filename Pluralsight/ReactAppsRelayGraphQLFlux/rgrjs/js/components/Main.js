ec5def5bc2176e68d8a798e05dca4660b03c6acd
Earley <earleyp@TANDBERGTV.COM>
2018-08-08 08:39:32 +0100

    Relay: Working with Variables

--- Pluralsight/ReactAppsRelayGraphQLFlux/rgrjs/js/components/Main.js
+++ Pluralsight/ReactAppsRelayGraphQLFlux/rgrjs/js/components/Main.js
@@ -6,6 +6,11 @@ import Link from "./Link";
 
 class Main extends React.Component {
   // Class member syntax requires babel-preset-stage-0 for advanced features
+  setLimit = (e) => {
+    let newLimit = Number(e.target.value);
+    this.props.relay.setVariables({limit: newLimit});
+  }
+
   render() {
     let content = this.props.store.links.map(link => {
       return  <Link key={link._id} link={link} />
@@ -14,6 +19,10 @@ class Main extends React.Component {
     return (
       <div>
         <h3>Links</h3>
+        <select onChange={this.setLimit}>
+          <option value="1">1</option>
+          <option value="2">1</option>
+        </select>
         <ul>
           {content}
         </ul>
@@ -24,12 +33,19 @@ class Main extends React.Component {
 
 // Declare the data requirements for this component
 Main = Relay.createContainer(Main, {
+  initialVariables: {
+    limit:2
+  },
   fragments: {
     store: () => Relay.QL`
       fragment on Store {
-        links {
-          _id,
-          ${link.getFragment('link')}
+        linkConnection(first: $limit) {
+          edges {
+            node {
+              id,
+              ${link.getFragment('link')}
+            }
+          }
         }
       }`
   }
