4ce8c3df0ffe5abadf8a235638938615c280633d
Earley <earleyp@TANDBERGTV.COM>
2018-08-17 08:17:23 +0100

    Using a Relay Mutation

--- Pluralsight/ReactAppsRelayGraphQLFlux/rgrjs/js/components/Main.js
+++ Pluralsight/ReactAppsRelayGraphQLFlux/rgrjs/js/components/Main.js
@@ -2,6 +2,7 @@ import React from "react";
 import Relay from "react-relay";
 
 import Link from "./Link";
+import CreateLinkMutation from "../mutations/CreateLinkMutation";
 
 
 class Main extends React.Component {
@@ -9,6 +10,19 @@ class Main extends React.Component {
   setLimit = (e) => {
     let newLimit = Number(e.target.value);
     this.props.relay.setVariables({limit: newLimit});
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault();
+    Relay.Store.Update(
+      new CreateLinkMutation({
+        title: this.refs.newTitle.value,
+        url: this.refs.newUrl.value,
+        store: this.props.store
+      })
+    );
+    this.refs.newTitle.value = "";
+    this.refs.newUrl.value = "";
   }
 
   render() {
@@ -19,7 +33,13 @@ class Main extends React.Component {
     return (
       <div>
         <h3>Links</h3>
-        <select onChange={this.setLimit}>
+        <form onSubmit={this.handleSubmit}>
+          <input type="text" placeholder="Title" ref="newTitle" />
+          <input type="text" placeholder="Url" ref="newUrl" />
+          <button type="submit">Add</button>
+          Showing: &nbsp;
+        </form>
+        <select onChange={this.setLimit} defaulValue={this.props.relay.variables.limit} >
           <option value="1">1</option>
           <option value="2">1</option>
         </select>
@@ -39,6 +59,7 @@ Main = Relay.createContainer(Main, {
   fragments: {
     store: () => Relay.QL`
       fragment on Store {
+        id,
         linkConnection(first: $limit) {
           edges {
             node {
